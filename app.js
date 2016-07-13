53750f2e42a5380d06a84ecd99897793dd0684d0
COREY MALEK <COREYMALEK1@GMAIL.COM>
2016-07-13 11:19:15 -0700

    added recent projects from newest to oldest

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-// $('.icon').on('click', function() {
-//   $(this).slideToggle();
-//   $(this).toggleClass('active');
-//   console.log('yes');
-// });
-// console.log('yes');
 
-$('.icon').on('click', function(){
-  $('ul').slideToggle('slow');
-  $(this).toggleClass('active');
-});
-
-var hamburger = $('.fa-bars').css('display');
-// console.log('yes');
-// if (hamburger == 'none') {
-//   $('.nav').css('display', 'inline');
-//   console.log('yes');
-// }
-
-if (!hamburger.localeCompare('none')) {
-  $('.nav').css('display', 'inline');
-  console.log('yes');
-}
-
-click.isDefaultPrevented();
+  $('.icon').on('click', function(){
+    $('.nav').slideToggle('slow');
+    $(this).toggleClass('active');
+  });
