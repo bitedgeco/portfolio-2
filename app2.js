4629eb6fbbde206b56709845ef930b5df111c2d4
COREY MALEK <COREYMALEK1@GMAIL.COM>
2016-07-15 10:30:26 -0700

    MVP class-04 portfolio assignment

--- app2.js
+++ app2.js
@@ -8,18 +8,24 @@ function Repos (opts) {
 }
 
 Repos.prototype.toHtml = function () {
-  var $newProject = $('article.template').clone();
+  var source = $('#repos-template').html();
+  var template = Handlebars.compile(source);
+  // var $newProject = $('article.template').clone();
   // $newProject.attr('id', this.title);
-  $newProject.find('h1.project-title').text(this.title);
-  $newProject.find('a.author-url').text(this.author);
-  $newProject.find('a.author-url').attr('href', this.authorUrl);
-  $newProject.find('time').attr('pubdate', this.publishedOn);
-  $newProject.find('time[pubdate]').attr('title', this.publishedOn);
-  $newProject.find('time').html('about ' + parseInt((new Date() - new
-  Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  // $newProject.find('h1.project-title').text(this.title);
+  // $newProject.find('a.author-url').text(this.author);
+  // $newProject.find('a.author-url').attr('href', this.authorUrl);
+  // $newProject.find('time').attr('pubdate', this.publishedOn);
+  // $newProject.find('time[pubdate]').attr('title', this.publishedOn);
+  // $newProject.find('time').html('about ' + parseInt((new Date() - new
+  // Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  //
+  // $newProject.removeClass('template');
+  // return $newProject;
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
+  this.publishTime = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  $newProject.removeClass('template');
-  return $newProject;
+  return template(this);
 };
 
 ourLocalData.sort(function(a,b) {
@@ -41,12 +47,17 @@ var portfolioView = {};
 portfolioView.handleMainNav = function() {
   $('section.tab-content section').hide();
   $('.main-nav').on('click', '.tab', function(){
-    var $dataContent = $(this).attr('data-content');
-    $('section.tab-content section').hide();
-    $('section#' + $dataContent).show();
-
-
-
+    var a = $('section#repos').css('display');
+    var b = $('section#about').css('display');
+    if((a === 'none') && (b === 'none')) {
+      console.log(a,b);
+      var $dataContent = $(this).attr('data-content');
+      $('section.tab-content section').hide();
+      $('section#' + $dataContent).show();
+    } else {
+      $('section#repos').hide();
+      $('section#about').hide();
+    }
 
   });
 };
